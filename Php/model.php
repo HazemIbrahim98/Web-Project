0c83b812cd2afde48c64d63f5c6709ddae0ad52a
drakenz2010 <drakenz2010@gmail.com>
2021-06-01 22:50:30 +0200

    Fixes for Results.html

--- Php/model.php
+++ Php/model.php
@@ -8,7 +8,10 @@ try {
 
     session_start();
     $_SESSION["modelOutput"] = $output;
-
+    $str_explode = explode("\n", $output)
+    echo '<script type="text/javascript">';
+    echo "sessionStorage.setItem('modelout','". $str_explode[0] ."');";
+    
     echo json_encode($output);
 } catch (Exception $e) {
     echo $e;
