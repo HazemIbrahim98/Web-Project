46a07d1471392ea737d842eeace61080c5e830d3
drakenz2010 <drakenz2010@gmail.com>
2021-06-01 17:39:33 +0200

    Added Folder for each user so we can serve multiple user at once using session variables

--- Php/storeCSV.php
+++ Php/storeCSV.php
@@ -1,8 +1,18 @@
 <?php
 if (!empty($_POST['data'])) {
     $data = $_POST['data'];
-
-    $file = fopen("../PythonBackend/" . "DATASET.csv", 'w');
-    fwrite($file, $data);
-    fclose($file);
+    session_start();
+    try
+    {
+        $path = "../Dataset/".$_SESSION["name"]."/dataset.csv";
+        $file = fopen($path, 'w');
+        fwrite($file, $data);
+        fclose($file);
+    }
+    catch(Exception $e)
+    {
+        echo $e;
+    }
+    
+    
 }
