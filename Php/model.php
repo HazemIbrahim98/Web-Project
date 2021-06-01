1bc6e2c40b143e4e339471bf148f0fff9f5b9361
drakenz2010 <drakenz2010@gmail.com>
2021-06-01 15:11:33 +0200

    Added Accuracy metrics in Decision Tree Classifier

--- Php/model.php
+++ Php/model.php
@@ -6,7 +6,7 @@ try {
     $com = 'python ../PythonBackend/runner_script.py ' . $args;
     $command = escapeshellcmd($com);
     $output = shell_exec($command);
-
+    
     echo json_encode($output);
 } catch (Exception $e) {
     echo $e;
